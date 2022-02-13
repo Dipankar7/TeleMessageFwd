28b8285123f29055a452c3ba5e7ddb65066c9705
Snehal Chavare <dipankarcool7@gmail.com>
2022-02-13 09:17:52 +0530

    first commit

---
+++ teleMain.py
@@ -0,0 +1,62 @@
+import os
+
+from telethon import TelegramClient, events
+from decouple import config
+import logging
+from telethon.sessions import StringSession
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+
+print("Starting...")
+
+# Basics
+#APP_ID = config("APP_ID", default=None, cast=int)
+APP_ID = "19859411"
+#API_HASH = config("API_HASH", default=None)
+API_HASH = "eb27349b19f7020edb828118b45f647d"
+#FROM_ = config("FROM_CHANNEL")
+FROM_ = "1403175761 1249862622"
+#TO_ = config("TO_CHANNEL")
+TO_ = ""
+#string = os.environ.get('SESSION')
+string = None
+
+FROM = [int(i) for i in FROM_.split()]
+TO = [int(i) for i in TO_.split()]
+
+try:
+    Bot = TelegramClient(StringSession(string), APP_ID, API_HASH)
+    Bot.start()
+except Exception as ap: 
+    print(f"ERROR - {ap}")
+    exit(1)
+
+@Bot.on(events.NewMessage(incoming=True, chats=FROM))
+#@Bot.on(events.NewMessage(incoming=True))
+async def send(event):
+    print(event)
+    print(event.text)
+    for i in TO:
+        try:
+            if event.poll:
+                return
+            if event.photo:
+                photo = event.media.photo
+                await Bot.send_file(i, photo, caption = event.text, link_preview = False)
+            elif event.media:
+                try:
+                    if event.media.webpage:
+                        await Bot.send_message(i, event.text, link_preview = False)
+                        return
+                except:
+                    media = event.media.document
+                    await Bot.send_file(i, media, caption = event.text, link_preview = False)
+                    return
+            else:
+                await Bot.send_message(i, event.text, link_preview = False)
+
+        except Exception as e:
+            print(e)
+
+print("Bot has started.")
+Bot.run_until_disconnected()
